955bfd03746011150e9b1b2dbd2ce066eab13712
Daniel Ocampo <ochampo77@gmail.com>
2021-09-23 23:11:44 -0700

    created list in js

--- src/App.js
+++ src/App.js
@@ -5,31 +5,54 @@ function Header (props)
 {
 
   return(
-    <header>    
-      <h1>{props.name} Kitchen</h1>
-      <p>Copy right  {props.year}</p>
+    <header>
+      <h1>{props.name} Testing</h1>
     </header>
   );
 }
 
-function Main(){
+function Main(props){
 
   return (
 <section>
 
-  <p>React is kinda intresting</p>
+  <p style={{textAlign: "left"}} >{props.adjective} </p>
+  <ul style={{textAlign: "left"}}>
+  {props.benefitsOfFunctionalCode.map((benefitsOfFunctionalCode) =>(
+  
+  <li key={benefitsOfFunctionalCode.id}>{benefitsOfFunctionalCode.title}</li>
+  ))}
+  </ul>
+</section>
+  );
+}
+function Footer(props){
+
+  return (
+<section>
+
+  <footer>Copyright {props.year} </footer>
 </section>
   )
 }
 
+const benefitsOfFunctionalCode = [
+  "Cleaner Code",
+  "makes debugging easier",
+  "Dynamic"
+  ];
+
+  const ObjectsOfBenefitsOfFunctional = benefitsOfFunctionalCode.map((benefitsOfFunctionalCode,i) => ({id:i, title: benefitsOfFunctionalCode}))
+  //console.log(ObjectsOfBenefitsOfFunctional)
 
 function App() {
   return (
     <div className="App">
-      <header  name = "cindy"/>
-      <Main adjective = "amazing"/>
-      <footer year={new Date().getFullYear()} />
-      <h3> header 3 </h3>
+      <Header name="Daniel" />
+      <Main adjective = "The benefit of functional code is " benefitsOfFunctionalCode={ObjectsOfBenefitsOfFunctional} />
+      <Footer year = {new Date().getFullYear()} />
+      
+      
       
     </div>
   );
