9381a5e73d464defebf921d1aaa6346749968d3d
Daniel Ocampo <ochampo77@gmail.com>
2021-11-08 00:42:07 -0800

    created an api call, and pulled data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import React ,{useState, useEffect, useReducer} from "react";
+import React ,{useState, useEffect} from "react";
 import './App.css';
 
 
+//https://api.github.com/users
 
+function App({login}) {
 
-function App() {
-const [checked, setChecked] = useState(false);
-const [checked2, toggle2] = useReducer(
-  (checked2) => !checked2,
-  false
+const [data, setData] = useState(null);
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(null);
+useEffect(() => {
+  if(!login) return;
+  setLoading(true);
+  fetch(`https://api.github.com/users/${login}`)
+  .then((response) => response.json())
+  .then(setData)
+  .then(() => setLoading(false))
+  .catch(setError);
+
+},[login]);
+
+if(loading) return <h1> loading ...</h1>
+if(error) return <pre>{JSON.stringify(error,null,2)}</pre>
+if(!data) return null;
+if(data)
+{
+  //return <div>{JSON.stringify(data)}</div>
+  return(
+  <div>
+    <h1>{data.name}</h1>
+    <p>{data.location}</p>
+    <img alt = {data.login} src = {data.avatar_url} />
+  </div>
   );
-  
-function toggle1(){
-  setChecked ((checked) => !checked)
 }
 
+return <div>No user data</div>
 
-return(
- 
-<>
-<input type="checkbox" 
-Value={checked2} 
-onChange={toggle2}
-/>
-<p> {checked2 ? "checked":"not checked"} </p>  
-</>
-);
+
 }
-  
-  
-
-
+    
 export default App;
